064361ffbcb40e25c70aa8976d3e2812a1addb6a
jozyToby <45020581+jozyToby@users.noreply.github.com>
2020-10-07 15:34:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const classNames = {
+  TODO_ITEM: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
+  TODO_TEXT: 'todo-text',
+  TODO_DELETE: 'todo-delete',
+}
+
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+function newTodo() {
+  alert('New TODO button clicked!')
+  let new_item = prompt('add a new item')
+  list.appendChild(new_item);
+}
